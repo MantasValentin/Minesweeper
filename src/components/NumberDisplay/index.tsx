2f6d3e3dca89dab82188be4d6a76d2ef92ec1465
MantasValentin <mantasvalentin@protonmail.com>
2022-12-14 17:33:50 +0200

    first commit

---
+++ src/components/NumberDisplay/index.tsx
@@ -0,0 +1,17 @@
+import "./index.scss";
+
+interface Props {
+  value: number;
+}
+
+const NumberDisplay: React.FC<Props> = ({ value }) => {
+  return (
+    <div className="NumberDisplay">
+      {value < 0
+        ? `-${Math.abs(value).toString().padStart(2, "0")}`
+        : value.toString().padStart(3, "0")}
+    </div>
+  );
+};
+
+export default NumberDisplay;
